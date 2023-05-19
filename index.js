67fd47813ba55257e6cbcdb990470dd0ca90ad9b
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-05-19 18:10:36 +0600

    implement the delete function and update addToys get function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -43,9 +43,20 @@ async function run() {
     })
 
     app.get("/addToys", async (req, res) => {
-      const result = await addToyCollection.find().toArray();
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await addToyCollection.find(query).toArray();
       res.send(result);
     });
+
+    app.delete('/addToys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addToyCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
